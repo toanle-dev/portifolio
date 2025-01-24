b7e77752f53b4e855e5b44bce672b258bf55b232
Toan <toan.dev@outlook.com>
2025-01-24 15:28:49 -0300

    deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
   },
 ];
